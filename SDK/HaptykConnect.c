992bc4e2af4b970584a7f1e4d4184894bab8f07b
rameyk <kameron.ramey@oit.edu>
2019-03-14 18:49:32 -0700

    fixed SDK compilation bugs. added command line options. continued development of main loop

--- SDK/HaptykConnect.c
+++ SDK/HaptykConnect.c
@@ -19,41 +19,76 @@ static char * target_dev = "E7:2F:7B:39:84:52"; // Haptyk BLE addr
 static char * addr_type = "random";       	// Haptyk BLE addr type
 static char * security_level = "low";		// Default Security
 
-void connect_cb(GIOChannel *io, GError * error, gpointer data){
-	GAttrib *attribute;
+static gboolean opt_char_read = FALSE;		// Service options	
+static gboolean opt_char_write = FALSE;
+static gboolean opt_listen = FALSE;
+static char * opt_value = NULL;
+static int opt_handle = -1;
+
+static GMainLoop * event_loop;
+static gboolean error = FALSE;			// error flag
+static GSourceFunc current_opt;			// used for handling passed operations
+
+
+
+static GOptionEntry gatt_options[] = {
+	{"char-read", 0, 0, G_OPTION_ARG_NONE, &opt_char_read, "Read a characteristic from peripheral", NULL},
+	{"char-write", 0, 0, G_OPTION_ARG_NONE, &opt_char_write, "write to characteristic at peripheral", NULL},
+	{"listen", 0, 0, G_OPTION_ARG_NONE, &opt_listen, "listen for notifications and indications", NULL},
+	{ NULL }
+};
+
+static GOptionEntry options[] = {
+
+	{"adapter", 'i', 0, G_OPTION_ARG_STRING, &source_dev, "set local bluetooth adapter", "hciX"},
+	{"peripheral", 'b', 0, G_OPTION_ARG_STRING, &target_dev, "set peripheral address", "MAC"},
+	{"addr-type", 't', 0, G_OPTION_ARG_STRING, &addr_type, "set address type", "[random | public]"},
+	{"security", 'l', 0, G_OPTION_ARG_STRING, &security_level, "set security level", "[low | medium | high]"},
+	{ NULL }
+};
+
+static GOptionEntry read_write_options = {
+
+	{"handle", 'a', 0, G_OPTION_ARG_INT, &opt_handle, "specify the handle to read/write", "0x0001"},
+	{"value", 'n', 0, G_OPTION_ARG_STRING, &opt_value, "Specify value to be written to characteristic", "0x0001"},
+	{ NULL }
+};
+
+void connect_cb(GIOChannel *io, GError * error, gpointer data){		/* Callback for determining connection */
+	GAttrib *attribute;						
 	
 	if (error){
 		g_printerr("%s\n", error->message);
-		//g_main_loop_quit(main_loop);
+		g_main_loop_quit(event_loop);
 	}
 	attribute = g_attrib_new(io);
 }
 
-void read_characteristic_cb(guint8 status, const guint8* pdu, guint16 plen,
+void read_characteristic_cb(guint8 status, const guint8* pdu, guint16 plen,	/* Callback for returning read characteristic */
 		gpointer user_data) {
 
-	uint8_t value[plen];
+	uint8_t value[plen];							
 	ssize_t vlen;
 	int i;
 
-	if (status != 0) {
+	if (status != 0) {	// the read failed print error												
 		g_printerr("Characteristic value/descriptor read failed: %s\n",
 							att_ecode2str(status));
 	}
 
-	vlen = dec_read_resp(pdu, plen, value, sizeof(value));
+	vlen = dec_read_resp(pdu, plen, value, sizeof(value));		// get the length of the returned characteristic data
 	if (vlen < 0) {
-		g_printerr("Protocol error\n");
+		g_printerr("Protocol error\n");				// if we got no data an error has occured	
 	}
 	g_print("Characteristic value/descriptor: ");
 	for (i = 0; i < vlen; i++)
-		g_print("%02x ", value[i]);
+		g_print("%02x ", value[i]);				// loop through the data array printing characteristic display
 	g_print("\n");
 }
 
-void connect_haptyk(char * UUID, char * security){
-	GOptionContext * context;
-	GOptionGroup * gatt_group, *params_group, *char_rw_group;
+int main(int argc, char * argv[]){      //(char * UUID, char * security){ 			/* This is the main connect loop for haptyk */
+	GOptionContext * context;				
+	GOptionGroup * gatt_group, *params_group, *char_rw_group; 
 	GIOChannel * channel;
 	GError * error;
 
@@ -64,15 +99,39 @@ void connect_haptyk(char * UUID, char * security){
 		security_level = strdup(security);
 	}
 	
-	//context = g_option_context_new(NULL);
+	context = g_option_context_new(NULL);
+	g_option_context_add_main_entries(context, options, NULL);
+	
+	/* GATT Commands */
+
+	gatt_group = g_option_group_new("gatt", "GATT commands", "Show all GATT commands", NULL, NULL);
+	g_option_context_add_group(context, gatt_group);
+	g_option_group_add_entries(gatt_group, gatt_options);
+
+	/* Services and Characteristics Arguments */
+	
+		
+
+	/* Characteristics Read/Write Arguments */
+
+
+
+
 	channel = gatt_connect(source_dev, target_dev, addr_type, 
 			security_level, 0, 0, connect_cb, &error);
+
+
+
+	if(error)
+		exit(EXIT_FAILURE);
+	else
+		exit(EXIT_SUCCESS);
 }
 
-gboolean read_characteristic(gpointer data){
-	GAttrib * attribute = data;
+gboolean read_characteristic(gpointer data){				/* Read from a passed characteristic */	
+	GAttrib * attribute = data;					
 	
-	gatt_read_char(attribute, 0x0001, read_characteristic_cb, attribute);
+	gatt_read_char(attribute, 0x0001, read_characteristic_cb, attribute); // pass the read char callback func
 	
 	return FALSE;
 }
