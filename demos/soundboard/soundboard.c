bc23ed8c8bc688044d526ffdd70cf7c2816bbca8
hdt80 <ifyoucanrememberthisforgetit@gmail.com>
2019-04-25 19:45:39 +0000

    Update SDK to include return codes. Fix soundboard demo

--- demos/soundboard/soundboard.c
+++ demos/soundboard/soundboard.c
@@ -4,10 +4,36 @@
 
 #include "haptyk.h"
 
-#define CLR_RESET	"\x1b[0m"
+#define CLR_RED		"\x1b[31m"
+#define CLR_GREEN	"\x1b[32m"
+#define CLR_YELLOW	"\x1b[33m"
+#define CLR_BLUE	"\x1b[34m"
 #define CLR_PURPLE	"\x1b[35m"
+#define CLR_CYAN	"\x1b[36m"
+#define CLR_RESET	"\x1b[0m"
 
-void logf(FILE* file, const char* tag, const char* color, const char* fmt, ...) {
+#if DEBUG
+	#pragma message "Building debug version"
+#endif
+
+#define infof(fmt, ...) \
+	do { msgf(stdout, "INFO", CLR_BLUE, fmt, ##__VA_ARGS__); } while (0)
+
+#define warnf(fmt, ...) \
+	do { msgf(stdout, "WARN", CLR_YELLOW, fmt, ##__VA_ARGS__); } while (0)
+
+#define errorf(fmt, ...) \
+	do { msgf(stderr, "ERRO", CLR_RED, fmt, ##__VA_ARGS__); } while (0)
+
+#if DEBUG
+	#define debugf(fmt, ...) \
+		do { msgf(stdout, "DBUG", CLR_PURPLE, fmt, ##__VA_ARGS__); } while(0)
+#else
+	#define debugf(fmt, ...) (void)0
+#endif
+
+
+void msgf(FILE* file, const char* tag, const char* color, const char* fmt, ...) {
 	fprintf(file, "%s[%s%s%s] ", CLR_RESET, color, tag, CLR_RESET);
 
 	va_list args;
@@ -16,12 +42,6 @@ void logf(FILE* file, const char* tag, const char* color, const char* fmt, ...) 
 	va_end(args);
 }
 
-
-#if DEBUG
-	#define debugf(F, __VA_ARGS__) logf(stdout, "DBUG", CLR_PURPLSE, F, A)
-#else
-	#define debugf(F, A) return
-#endif
 
 // Print usage for this demo
 //
@@ -50,23 +70,32 @@ int main(int argc, char** argv) {
 
 	// Load the sound config
 	if (load_sounds("config") == -1) {
+		errorf("Failed to load sound config file");
 		return 1;
 	}
 
 	const char* bt_addr = argv[1];
 
-	printf("Haptyk soundboard demo\n");
+	infof("Haptyk soundboard demo\n");
 
-	printf("Connecting to Haptyk device '%s'\n", bt_addr);
-	haptyk_init(bt_addr);
-	printf("Connected\n");
+	infof("Connecting to Haptyk device '%s'\n", bt_addr);
+	uint8_t init_code = haptyk_init(bt_addr);
+	debugf("Return status from haptyk_init %d\n", init_code);
+	if (init_code != 0) {
+		errorf("Failed to connect to '%s'\n", bt_addr);
+		return 1;
+	}
+	infof("Connected\n");
 
 	struct haptyk_buttons_t prev_data, curr_data;
 
 	haptyk_get_data(&curr_data);
 
 	while (1) {
-		haptyk_get_data(&prev_data);
+		uint8_t ret_code;
+		if ((ret_code = haptyk_get_data(&prev_data)) != 0) {
+			errorf("Error reading button data from '%s'\n", bt_addr);
+		}
 	}
 
 	return 0;
@@ -76,7 +105,7 @@ uint8_t load_sounds(const char* config_file) {
 	FILE* sound_file = fopen(config_file, "r");
 
 	if (sound_file == NULL) {
-		fprintf(stderr, "Failed to open config file '%s'\n", config_file);
+		warnf("Failed to open config file '%s'\n", config_file);
 		return -1;
 	}
 
@@ -87,10 +116,12 @@ uint8_t load_sounds(const char* config_file) {
 	uint8_t sound_index = 0;
 	while ((read = getline(&line, &len, sound_file)) != -1) {
 		debugf("%d => '%s'\n", sound_index, line);
+
+		free(line);
+		line = NULL;
 	}
 
 	fclose(sound_file);
-	free(line);
 
 	return sound_index;
 }
